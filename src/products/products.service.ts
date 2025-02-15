685ab00a922ff3ff70f37d305bc225b35a284675
mufrick01 <mrmufrick@gmail.com>
2025-02-15 03:51:49 -0300

    changed from api rest to microservice

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from '../common/dto/pagination.dto';
+
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -13,23 +15,79 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     this.$connect();
     this.logger.log(`DataBase connected`)
   }
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.product.create({
+      data:createProductDto
+    })
+
+    return product;
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto:PaginationDto) {
+
+    const {page,limit} = paginationDto;
+
+    const totalProducts = await this.product.count({where:{available:true}});
+    const lastPage = Math.ceil(totalProducts/limit);
+
+
+    const data = await this.product.findMany({
+      where:{available:true},
+      take:limit,
+      skip: (page-1)*limit
+    });
+
+    return {
+      meta:{
+        total: totalProducts,
+        page,
+        lastPage
+      },
+      data
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({where:{id,available:true}})
+    if(!product){
+      throw new NotFoundException(`product with id ${id} not found`)
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const {id:_,...data} = updateProductDto;
+    try {
+      const product = await this.product.update({where:{id, available:true},data})
+      return product;
+
+    } catch (error) {
+      this.handleErrors(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    try {
+      return await this.product.update(
+      {
+        where:{id, available:true},
+        data:{ available:false }
+      }
+    );
+    } catch (error) {
+      this.handleErrors(error)
+    }
   }
+
+  private handleErrors(error:any){
+    if(error.code === "P2025"){
+      throw new BadRequestException(`product to update or delete not found`)
+    }
+    this.logger.error(error.meta.cause)
+    throw new InternalServerErrorException('Something failed, check logs')
+  }
+
+
 }
